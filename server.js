0bf2b8564dac8138cba1df515fa71a7536f98200
hassanrotich111-source <hassanrotich111@gmail.com>
2026-04-03 09:38:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -255,5 +255,7 @@ app.get('/api/inventory', (req, res) => {
   });
   res.json(Object.values(inventory));
 });
-
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
