0d3fc8c7fad715e4a8c4966317f696b15d4971e9
hassanrotich111-source <hassanrotich111@gmail.com>
2026-04-06 09:51:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -255,13 +255,5 @@ app.get('/api/inventory', (req, res) => {
   });
   res.json(Object.values(inventory));
 });
-app.get('/', (req, res) => {
-  res.send('API is running 🚀');
-});
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-app.get('/latest', (req, res) => {
-  res.json({ message: "Latest endpoint working" });
-});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
